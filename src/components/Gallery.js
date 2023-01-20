da1bb8287f424de286db2a4bce4b7bc789d06f5a
ThiagoSartorel <thiagosartorel@uniplac.net>
2023-01-20 16:49:59 -0300

    Inicio calendario

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,6 @@
 export default function Gallery() {
   return (
-    <div class="flex flex-col items-center justify-center  ">
+    <div class="flex flex-col items-center justify-center  " id="fotos">
       <div
         class="grid grid-flow-row gap-4 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <div className="  card w-96 bg-base-100 shadow-xl p-2 rounded-md">

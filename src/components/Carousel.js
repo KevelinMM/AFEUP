99cab2dd95b5c154090c4a2a68d5a3f31f765bff
Uniplac <github@uniplaclages.edu.br>
2023-01-20 14:07:03 -0300

    image error

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,26 +1,27 @@
 import { Carousel } from "flowbite-react";
+import Image from "next/image";
 
 export default function carousel() {
   return (
 <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
   <Carousel slideInterval={4000}>
-    <img
+    <Image
       src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
       alt="..."
     />
-    <img
+    <Image
       src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
       alt="..."
     />
-    <img
+    <Image
       src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
       alt="..."
     />
-    <img
+    <Image
       src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
       alt="..."
     />
-    <img
+    <Image
       src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
       alt="..."
     />

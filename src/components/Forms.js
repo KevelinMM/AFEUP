8d512c75b3e9ea5264d19806e216189cd8780a28
Uniplac <github@uniplaclages.edu.br>
2023-01-24 17:08:59 -0300

    formulario

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,30 +1,28 @@
 export default function Forms() {
     return (
 
-<form>
-<h2>Entre em contato</h2>
+<section className="md:flex ">
+  <div className="py-8 lg:py-12 px-4 mx-auto ">
+     
+      <h2>Entre em contato para cadastro ou locação</h2>
+      <form action="#" className="space-y-8">
+          <div>
+              <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Seu email</label>
+              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " placeholder="name@flowbite.com" required/>
+          </div>
+          <div>
+              <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 ">Data de locação</label>
+              <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 " placeholder="Selecione a data" required/>
+          </div>
+          <div className="sm:col-span-2">
+              <label for="message" className="block mb-2 text-sm font-medium text-gray-900 ">Your message</label>
+              <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Deixe seu comentário"></textarea>
+          </div>
+          <button type="submit" className="button">Enviar</button>
+      </form>
+  </div>
 
-  <div className="mb-6" id="contato">
-    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Seu nome</label>
-    <input type="email" id="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="name@flowbite.com" required/>
-  </div>
-  <div className="mb-6">
-    <label for="text" className="block mb-2 text-sm font-medium text-gray-900 ">Seu email</label>
-    <input type="text" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required/>
-  </div>
-  <div className="mb-6">
-    <label for="repeat-password" className="block mb-2 text-sm font-medium text-gray-900 ">Mensagem</label>
-    <input type="password" id="mensage" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required/>
-  </div>
-  <div className="flex items-start mb-6">
-    <div className="flex items-center h-5">
-      <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300   " required/>
-    </div>
-    <label for="terms" className="ml-2 text-sm font-medium text-gray-900 ">Li e aceito os <a href="#" className="text-blue-600 hover:underline ">termos de Política de Privacidade.</a></label>
-  </div>
-  <button >Enviar</button>
-
-</form>
+</section>
 
     );
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,37 @@ export default function Home() {
       <h1 className="page-title" id="sobre">
         Associação dos Funcionários da UNIPLAC
       </h1>
-      <div className="page-content mb-14">
-        <h2>Sobre nós</h2>
-        <p className="px-4">
-          A Associação dos Funcionários da UNIPLAC, é uma entidade que
-          congregará todos os funcionários que a ela se vinculem. É uma
-          sociedade civil, com personalidade jurídica de duração indeterminada,
-          designada pela sigla AFEUP.
-        </p>
-        <h2>Finalidades</h2>
-        <p className="pl-4 text">
-        
-           Defender os interesses dos seus associados.
-           <br></br>
-          Zelar pela dignidade e prestígio dos associados. 
-          <br></br>
-          Estimular entre os associados o sentido do coleguismo e amizade. 
-          <br></br>
-          Promover o congraçamento social e recreativo entre os membros. 
-          <br></br>
-          Disponibilizar convênios de serviços empresariais aos associados.
-        </p>
+      <div className="page-content mb-14 ">
+        <div className=" ">
+          <div className="">
+            <h2>Sobre nós</h2>
+            <p className="px-4">
+              A Associação dos Funcionários da UNIPLAC, é uma entidade que
+              congregará todos os funcionários que a ela se vinculem. É uma
+              sociedade civil, com personalidade jurídica de duração
+              indeterminada, designada pela sigla AFEUP.
+            </p>
+          </div>
+          <div>
+            <h2>Finalidades</h2>
+            <p className="pl-4 ">
+              Defender os interesses dos seus associados.
+              <br></br>
+              Zelar pela dignidade e prestígio dos associados.
+              <br></br>
+              Estimular entre os associados o sentido do coleguismo e amizade.
+              <br></br>
+              Promover o congraçamento social e recreativo entre os membros.
+              <br></br>
+              Disponibilizar convênios de serviços empresariais aos associados.
+            </p>
+          </div>
+        </div>
 
         <h2>Nosso espaço</h2>
 
         <Gallery id="fotos" />
-     
+
         <Forms id="contato" />
       </div>
     </section>

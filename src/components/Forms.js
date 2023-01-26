8ece7a8a2ea474146392cb3f75b2778f954f123e
Uniplac <github@uniplaclages.edu.br>
2023-01-26 10:54:10 -0300

    teste

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,7 +1,7 @@
 export default function Forms() {
   return (
     <section className="md:flex" id="contato">
-      <div className="grid grid-cols-1 lg:grid-cols-2 p-2  md:mx-12 lg:mx-44 space-x-6 w-full ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 p-2 md:mx-12 lg:mx-44 space-x-6 w-full ">
         <div className="h-full bg-slate-200 bg-opacity-90  rounded-xl  mx-4">
           <img
             className="mx-auto "
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,136 +1,136 @@
 export default function Gallery() {
   return (
     <div className=" flex flex-col items-center justify-center  " id="fotos">
-      <div className=" grid grid-flow-row gap-4 text-neutral-600 cursor-pointer sm:grid-cols-2  xl:grid-cols-4 ">
-        <div className=" w-72  shadow-xl   ">
+      <div className=" grid grid-flow-row gap-4 text-neutral-600 cursor-pointer sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 ">
+        <div className=" w-96  shadow-xl   ">
           <figure>
             <img
               src="../FotosLocal/area.jpeg"
               alt="Área externa"
               data-fancybox="gallery"
               data-caption="Área externa"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/campo.jpeg"
               alt="Campo de Futebol"
               data-fancybox="gallery"
               data-caption="Campo de Futebol"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/campoExt.jpeg"
               alt="rea externa"
               data-fancybox="gallery"
               data-caption="Área externa"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/casa1.jpeg"
               alt="Casa"
               data-fancybox="gallery"
               data-caption="Casa"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/casa2.jpeg"
               alt="Casa"
               data-fancybox="gallery"
               data-caption="Casa"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/casa3.jpeg"
               alt="Casa"
               data-fancybox="gallery"
               data-caption="Casa"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/casaArea.jpeg"
               alt="Casa"
               data-fancybox="gallery"
               data-caption="Casa"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/entrada1.jpeg"
               alt="Entrada"
               data-fancybox="gallery"
               data-caption="Entrada"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/entrada2.jpeg"
               alt="Entrada"
               data-fancybox="gallery"
               data-caption="Entrada"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className=" w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/parquinho.jpeg"
               alt="Parquinho"
               data-fancybox="gallery"
               data-caption="Parquinho"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/piscina.jpeg"
               alt="Piscina"
               data-fancybox="gallery"
               data-caption="Piscina"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
-        <div className="w-72 shadow-xl">
+        <div className="w-96 shadow-xl">
           <figure>
             <img
               src="../FotosLocal/piscina2.jpeg"
               alt="Piscina"
               data-fancybox="gallery"
               data-caption="Piscina"
-              className="gallery"
+              className="rounded-md"
             />
           </figure>
         </div>
